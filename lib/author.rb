ca1aa823076b0261f62f99796b67481c49561c00
jacobgwiggins <jacobgwiggins9@gmail.com>
2017-09-24 01:09:57 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,15 +1,31 @@
-class Author
+class Artist
   attr_accessor :name
+
+  @@song_count = 0
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
-  def add_post_by_title(title)
-    post = Post.new(title, self)
+  def add_song(song)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
   end
 
-  def posts
-    Post.all.select { |post| post.author == self }
+  def add_song_by_name(name)
+    song = Song.new(name)
+    self.songs << song
+    song.artist = self
+    @@song_count +=1
+  end
+
+  def songs
+    @songs 
+  end
+
+  def self.song_count
+    @@song_count
   end
 end
